dec9d2b0eabc24a190db15616d8a6c32c3258ed5
arpitsha26 <arpitsharma1263@gmail.com>
2025-05-08 09:16:03 +0530

    express server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const http = require('http');
+const fs=require('fs')
+const url = require('url');
+const express= require('express');
 
-const myServer = http.createServer((req,res)=>{
+const app= express();
+app.get('/',(req,res)=>{
+    return res.send("hello from home page"
+    )
+})
+
+app.get('/about', (req,res)=>{
+    return res.send(`Hello mr ${req.query.name}`)
+})
+/*const myServer = http.createServer((req,res)=>{
     console.log("New req rec. ");
     res.end("Hello from Server");
  });
  myServer.listen(8000, ()=> console.log("Server Started"));
+ */
+
+ const myServer=http.createServer(app);
+ myServer.listen(8000, ()=>console.log("server started"));
