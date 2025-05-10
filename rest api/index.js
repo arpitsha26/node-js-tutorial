943efa3b074551b7bcbd13778cb08f64cece14ec
arpitsha26 <arpitsharma1263@gmail.com>
2025-05-10 23:17:54 +0530

    http status codes

--- rest api/index.js
+++ rest api/index.js
@@ -40,9 +40,14 @@ app.get('/api/users',(req,res)=>{
 
 app.post('/api/users',(req,res)=>{
     const body=req.body;
+    if(!body || !body.first_name|| !body.email || !body.last_name || !body.gender || !body.job_title){
+        return res.status(400).json({
+            mssg: "All feilds are required"
+        })
+    }
     users.push({...body, id: users.length+1})
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
-     return res.json({status: "ok", 
+     return res.status(201).json({status: "ok", 
                        id: users.length
      });
     })
@@ -55,6 +60,11 @@ app.route('/api/user/:id')
 .get((req,res)=>{
     const id=Number(req.params.id);
     const user=users.find((user)=>user.id==id ) ;
+    if(!user){
+        return res.status(404).json({
+            error: "user not found"
+        })
+    }
     return res.json(user);
 })
 .patch((req,res)=>{
